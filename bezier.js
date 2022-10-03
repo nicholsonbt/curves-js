13d5e2a06a0ddb1c19ee6ceed6541f6781519038
Ben Nicholson <bennicholson777@gmail.com>
2022-10-03 12:44:43 +0100

    Scale and offset curve to fill canvas

--- bezier.js
+++ bezier.js
@@ -1,6 +1,6 @@
 var BEZIER = {};
 BEZIER.controls = [];
-BEZIER.steps = 100;
+BEZIER.steps = null;
 BEZIER.camera = null;
 BEZIER.scene = null;
 BEZIER.renderer = null;
@@ -17,11 +17,11 @@ BEZIER.Setup = function(width, height) {
 	
 	document.body.appendChild(BEZIER.renderer.domElement);
 	
-	BEZIER.DrawCurve();
+	BEZIER.Draw();
 }
 
 BEZIER.SetupCamera = function() {
-	BEZIER.camera = new THREE.PerspectiveCamera(75, BEZIER.width / BEZIER.height, 1, 1000);
+	BEZIER.camera = new THREE.OrthographicCamera(-BEZIER.width / 2, BEZIER.width / 2, BEZIER.height / 2, -BEZIER.height / 2, 1, 1000)
 	BEZIER.camera.position.z = 100;
 }
 
@@ -31,10 +31,51 @@ BEZIER.SetupRenderer = function() {
 	BEZIER.renderer.setSize(BEZIER.width, BEZIER.height);
 }
 
-
-BEZIER.DrawCurve = function() {
-	BEZIER.controls = [{x : -10, y : -10}, {x : -10, y : 10}, {x : 10, y : 10}, {x : 10, y : -10}, {x: 20, y: 0}];
+BEZIER.GetScaleAndOffset = function() {
+	let size = BEZIER.GetSize(BEZIER.controls);
+	let scale = BEZIER.GetScale(size);
+	let offset = BEZIER.GetOffset(size);
 	
+	return [scale, offset];
+}
+
+
+BEZIER.Draw = function() {
+	BEZIER.controls = [{x : -10, y : -10}, {x : -10, y : 10}, {x : 10, y : 10}, {x : 10, y : -10}, {x: 20, y: 0}];
+	BEZIER.steps = 100;
+	
+	let so = BEZIER.GetScaleAndOffset()
+	
+	BEZIER.DrawControls(so[0], so[1]);
+	BEZIER.DrawCurve(so[0], so[1]);
+	
+	BEZIER.renderer.render(BEZIER.scene, BEZIER.camera);
+}
+
+
+BEZIER.DrawControls = function(scale, offset) {
+	var node;
+	
+	for (let i = 0; i < BEZIER.controls.length; i++) {
+		
+		node = new THREE.Mesh(
+			new THREE.SphereGeometry(5, 16, 16),
+			new THREE.MeshBasicMaterial( {color: new THREE.Color( 0xff0000 )} )
+		);
+		
+		node.position.set(
+			(BEZIER.controls[i].x - offset[0]) * scale,
+			(BEZIER.controls[i].y - offset[1]) * scale,
+			0
+		);
+		
+		BEZIER.scene.add(node);
+	}
+}
+
+
+
+BEZIER.DrawCurve = function(scale, offset) {
 	let curveGeometry = new THREE.BufferGeometry();
 	let curveMaterial = new THREE.LineBasicMaterial();
 	
@@ -52,14 +93,12 @@ BEZIER.DrawCurve = function() {
 	for (let step = 0; step <= BEZIER.steps; step += 1) {
 		node = BEZIER.GetNode(BEZIER.controls, step / BEZIER.steps);
 		
-		curve[3 * step + 0] = node.x;
-		curve[3 * step + 1] = node.y;
+		curve[3 * step + 0] = (node.x - offset[0]) * scale;
+		curve[3 * step + 1] = (node.y - offset[1]) * scale;
 		curve[3 * step + 2] = z;
 	}
 	
 	curveGeometry.setDrawRange(0, BEZIER.steps + 1);
-	
-	BEZIER.renderer.render(BEZIER.scene, BEZIER.camera);
 }
 
 BEZIER.GetNode = function(points, t) {
@@ -82,7 +121,29 @@ BEZIER.GetNode = function(points, t) {
 	return BEZIER.GetNode(newPoints, t);
 }
 
+BEZIER.GetSize = function(controls) {
+	let minX = Number.MAX_VALUE;
+	let minY = Number.MAX_VALUE;
+	let maxX = -Number.MAX_VALUE;
+	let maxY = -Number.MAX_VALUE;
+	
+	for (let i = 0; i < controls.length; i++) {
+		minX = Math.min(minX, controls[i].x);
+		minY = Math.min(minY, controls[i].y);
+		maxX = Math.max(maxX, controls[i].x);
+		maxY = Math.max(maxY, controls[i].y);
+	}
+	
+	return [minX * 1.5, minY * 1.5, maxX * 1.5, maxY * 1.5];
+}
 
-// window.addEventListener('load', function(e) {
-	// BEZIER.Setup(window.innerWidth, window.innerHeight);
-// });
+BEZIER.GetScale = function(size) {
+	widthScale = BEZIER.width / (size[2] - size[0]);
+	heightScale = BEZIER.height / (size[3] - size[1]);
+	
+	return Math.min(widthScale, heightScale);
+}
+
+BEZIER.GetOffset = function(size) {
+	return [(size[2] + size[0]) / 2, (size[3] + size[1]) / 2];
+}
